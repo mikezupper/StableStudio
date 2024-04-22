54a228482f254600c86e7dbd7d2f57040c81bd80
Mike Zupper <me@mikezupper.com>
2024-04-22 15:38:50 -0400

    rebrand to Dream

--- packages/stablestudio-ui/src/Theme/Logo/Next.tsx
+++ packages/stablestudio-ui/src/Theme/Logo/Next.tsx
@@ -5,7 +5,7 @@ export function Next() {
     <div className="flex items-center gap-1.5">
       <Logo/>
       <div className="flex flex-col">
-        <span className="text-lg font-medium">AI Generator</span>
+        <span className="text-lg font-medium">Dream</span>
         <span className="-mt-1 text-xs font-light">
           <span className="text-white/75">by </span>Livepeer.cloud
         </span>
